28ea2e0102f50399c9fc6cac1791ee4c776d1aa6
Selim Unal <trompet308@gmail.com>
2024-03-13 23:34:16 +0300

    The button structure is changed.

--- src/lib/components/buttons/button-1.component.ts
+++ src/lib/components/buttons/button-1.component.ts
@@ -10,13 +10,19 @@ import { TranslocoModule } from '@ngneat/transloco';
   imports: [TranslocoModule, NgTemplateOutlet, RouterLink, NgClass],
   template: `
     <div class="p-2">
-      @if (!!link()) {
-      <a [routerLink]="link()" [ngClass]="fill()" class="items-center gap-2 relative inline-flex justify-center px-4 py-2 overflow-hidden font-medium rounded-lg group bg-white">
+      @if (!!buttonOptions().link) {
+      <a
+        [routerLink]="buttonOptions().link"
+        [ngClass]="buttonOptions().specialClass"
+        class="items-center gap-2 relative inline-flex justify-center px-4 py-2 overflow-hidden font-medium rounded-lg group bg-white">
         <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-white/15 bg-opacity-30 rounded-full group-hover:w-56 group-hover:h-56"></span>
         <ng-container *ngTemplateOutlet="button1"></ng-container>
       </a>
       } @else {
-      <button (click)="clicked.emit($event)" class="items-center gap-2 relative inline-flex justify-center px-4 py-2 overflow-hidden font-medium rounded-lg group bg-white">
+      <button
+        [ngClass]="{ 'px-4 py-2': buttonOptions().hasPadding }"
+        (click)="clicked.emit($event)"
+        class="items-center gap-2 relative inline-flex justify-center px-4 py-2 overflow-hidden font-medium rounded-lg group bg-white">
         <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-slate-300 bg-opacity-30 rounded-full group-hover:w-56 group-hover:h-56"></span>
         <ng-container *ngTemplateOutlet="button1"></ng-container>
       </button>
@@ -24,22 +30,34 @@ import { TranslocoModule } from '@ngneat/transloco';
     </div>
 
     <ng-template #button1>
-      @if(!!icon()) {
-      <i [class]="icon()"></i>
-      } @if (!!title()) {
-      <span [ngClass]="{ 'text-base': size() === 'base', 'text-xs': size() === 'small', 'text-xl': size() === 'large' }">
-        {{ title() }}
+      @if(!!buttonOptions().icon) {
+      <i [class]="buttonOptions().icon"></i>
+      } @if (!!buttonOptions().title) {
+      <span [ngClass]="{ 'text-base': buttonOptions().size === 'base', 'text-xs': buttonOptions().size === 'small', 'text-xl': buttonOptions().size === 'large' }">
+        {{ buttonOptions().title }}
       </span>
       }
     </ng-template>
   `
 })
 export class Button1Component {
-  title = input<string>();
-  icon = input<string>();
-  fill = input<string>();
-  link = input<any[]>();
-  size = input<'small' | 'base' | 'large'>('large');
+  readonly #defaultButtonOptions = {
+    hasPadding: true,
+    size: 'base'
+  } as Button1Options;
+
+  buttonOptions = input<Button1Options, Button1Options>(this.#defaultButtonOptions, {
+    transform: (value) => ({ ...this.#defaultButtonOptions, ...value })
+  });
 
   @Output() clicked = new EventEmitter<any>();
+}
+
+interface Button1Options {
+  title?: string;
+  hasPadding?: boolean;
+  icon?: string;
+  specialClass?: string;
+  link?: any[];
+  size?: 'small' | 'base' | 'large';
 }
